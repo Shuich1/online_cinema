c032c4aa9c50638b17036c8890ae74098587d364
Shuichi <zhuravkov.mai@gmail.com>
2023-01-22 17:31:00 +0300

    Added film service

--- fastapi-solution/src/api/v1/films.py
+++ fastapi-solution/src/api/v1/films.py
@@ -1,7 +1,8 @@
 from http import HTTPStatus
 
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, Query
 from pydantic import BaseModel
+from typing import Union
 
 from services.film import FilmService, get_film_service
 
@@ -12,21 +13,26 @@ class Film(BaseModel):
     id: str
     title: str
 
+@router.get(
+    '/',
+    response_model=list[Film],
+    summary='All films',
+    description='Returns all films'
+)
+async def films(
+    film_service: FilmService = Depends(get_film_service),
+    sort: Union[str, None] = Query(None, description='Sort by something like imdb_rating', alias='sort'),
+    genre: Union[str, None] = Query(None, description='Filter by genre uuid', alias='filter[genre]',)
+    ) -> list[Film]:
+    films = await film_service.get_all(sort, genre)
+    if not films:
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail='films not found')
+    return films
 
-# Внедряем FilmService с помощью Depends(get_film_service)
 @router.get('/{film_id}', response_model=Film)
 async def film_details(film_id: str, film_service: FilmService = Depends(get_film_service)) -> Film:
     film = await film_service.get_by_id(film_id)
     if not film:
-        # Если фильм не найден, отдаём 404 статус
-        # Желательно пользоваться уже определёнными HTTP-статусами, которые содержат enum
-                # Такой код будет более поддерживаемым
         raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail='film not found')
 
-    # Перекладываем данные из models.Film в Film
-    # Обратите внимание, что у модели бизнес-логики есть поле description
-        # Которое отсутствует в модели ответа API.
-        # Если бы использовалась общая модель для бизнес-логики и формирования ответов API
-        # вы бы предоставляли клиентам данные, которые им не нужны
-        # и, возможно, данные, которые опасно возвращать
     return Film(id=film.id, title=film.title)
