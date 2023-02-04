2b4262d14d6e428b65ba9ee7eec4a273f21dec0b
vimjj <vimjj@yandex.ru>
2023-02-04 16:43:57 +0600

    moved service abc file, added methods

--- fastapi-solution/src/services/service.py
+++ fastapi-solution/src/services/service.py
@@ -3,11 +3,6 @@ from abc import ABC, abstractmethod
 
 class Service(ABC):
     @abstractmethod
-    def __init__(self, data_storage, cache_storage, *args, **kwargs):
-        self.data_storage = data_storage
-        self.cache_storage = cache_storage
-
-    @abstractmethod
     def get_by_id(self, uuid, *args, **kwargs):
         pass
 
@@ -17,4 +12,16 @@ class Service(ABC):
 
     @abstractmethod
     def search(self, query, *args, **kwargs):
+        pass
+
+    @abstractmethod
+    def _get_data_from_storage(self, *args, **kwargs):
+        pass
+
+    @abstractmethod
+    def _get_data_from_cache(self, *args, **kwargs):
+        pass
+
+    @abstractmethod
+    def _put_data_to_cache(self, *args, **kwargs):
         pass
