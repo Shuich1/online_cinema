34f4d9dbf7e34b56f6b704d13a998a2e335a494c
vimjj <vimjj@yandex.ru>
2023-02-04 16:42:17 +0600

    rework data storage abc class, added implementation of elastic

---
+++ fastapi-solution/src/db/data_storage.py
@@ -0,0 +1,42 @@
+from abc import ABC, abstractmethod
+from typing import Optional
+
+from elasticsearch import AsyncElasticsearch
+
+
+class DataStorage(ABC):
+    @abstractmethod
+    def search(self, *args, **kwargs):
+        pass
+
+    @abstractmethod
+    def get(self, *args, **kwargs):
+        pass
+
+    @abstractmethod
+    def close(self, *args, **kwargs):
+        pass
+
+
+class ElasticStorage(DataStorage):
+    def __init__(self, elastic: AsyncElasticsearch):
+        self.elastic = elastic
+
+    async def get(self, *args, **kwargs):
+        return await self.elastic.get(*args, **kwargs)
+
+    async def search(self, *args, **kwargs):
+        return await self.elastic.search(*args, **kwargs)
+
+    async def scroll(self, *args, **kwargs):
+        return await self.elastic.scroll(*args, **kwargs)
+
+    async def close(self):
+        await self.elastic.close()
+
+
+data_storage: Optional[DataStorage] = None
+
+
+async def get_data_storage() -> DataStorage:
+    return data_storage
