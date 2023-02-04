8fe19093d524bc7718c4db8611694d4348f228a9
vimjj <vimjj@yandex.ru>
2023-02-04 23:30:58 +0600

    services refactor

--- fastapi-solution/src/services/genre.py
+++ fastapi-solution/src/services/genre.py
@@ -1,80 +1,51 @@
 from functools import lru_cache
 from typing import Optional
 
-from elasticsearch import AsyncElasticsearch, NotFoundError
 from fastapi import Depends
 
-from src.db.elastic import get_elastic
 from src.db.cache import get_cache, Cache
+from src.db.data_storage import get_data_storage, DataStorage
 from src.models.genre import Genre
+from src.services.service import BaseService
 
 
-class GenreService:
-    def __init__(self, cache: Cache, elastic: AsyncElasticsearch):
-        self.cache = cache
-        self.elastic = elastic
+class GenreService(BaseService):
+    def __init__(self, cache: Cache, data_storage: DataStorage):
+        super().__init__(cache, data_storage)
 
-    async def get_by_id(self, genre_id: str) -> Optional[Genre]:
-        genre = await self._genre_from_cache(genre_id)
-        if not genre:
-            genre = await self._get_genre_from_elastic(genre_id)
-            if not genre:
-                return None
+    async def get_all(self,
+                      page_number: Optional[int],
+                      size: Optional[int]) -> Optional[list[Genre]]:
+        body = {
+                'query': {
+                        'match_all': {}
+                },
+        }
+        return await self._search(index='genres',
+                                  body=body,
+                                  page_number=page_number,
+                                  size=size,
+                                  model=Genre)
 
-            await self._put_genre_to_cache(genre)
-
-        return genre
-
-    async def get_all(self, page_number, size: Optional[int]) -> Optional[list[Genre]]:
-        try:
-            page = await self.elastic.search(
-                index='genres',
-                body={'query': {'match_all': {}}},
-                size=size,
-                scroll='2m',
-            )
-        except NotFoundError:
-            return []
-
-        scroll_id = page['_scroll_id']
-        hits = page['hits']['hits']
-
-        if page_number > 1:
-            for i in range(page_number - 1):
-                page = await self.elastic.scroll(
-                    scroll_id=scroll_id,
-                    scroll='2m'
-                )
-                scroll_id = page['_scroll_id']
-                hits = page['hits']['hits']
-
-        return [Genre(**hit['_source']) for hit in hits]
-
-    async def _get_genre_from_elastic(self, genre_id: str) -> Optional[Genre]:
-        try:
-            doc = await self.elastic.get('genres', genre_id)
-        except NotFoundError:
-            return None
-        return Genre(**doc['_source'])
-
-    async def _genre_from_cache(self, genre_id: str) -> Optional[Genre]:
-        data = await self.cache.get(f'genre_id:{genre_id}')
+    async def get_by_id(self, uuid: str) -> Optional[Genre]:
+        data = await self._get_data_from_cache(name_id='genre_id',
+                                               uuid=uuid,
+                                               model=Genre)
         if not data:
-            return None
+            data = await self._get_data_from_storage(index='genres',
+                                                     uuid=uuid,
+                                                     model=Genre)
+            if not data:
+                return None
+            await self._put_data_to_cache(name_id='genre_id',
+                                          data=data)
 
-        genre = Genre.parse_raw(data)
-        return genre
-
-    async def _put_genre_to_cache(self, genre: Genre):
-        await self.cache.set(
-            f'genre_id:{genre.id}',
-            genre.json()
-        )
+        return data
 
 
 @lru_cache()
 def get_genre_service(
         cache: Cache = Depends(get_cache),
-        elastic: AsyncElasticsearch = Depends(get_elastic),
+        data_storage: DataStorage = Depends(get_data_storage),
 ) -> GenreService:
-    return GenreService(cache, elastic)
+    return GenreService(cache, data_storage)
