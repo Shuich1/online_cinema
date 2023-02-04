5f52e88d76f6214bd1b53d43e51bb3457cbed90d
Shuichi <zhuravkov.mai@gmail.com>
2023-02-05 01:27:05 +0300

    Separated fixtures into modules

---
+++ fastapi-solution/tests/functional/fixtures/fastapi.py
@@ -0,0 +1,19 @@
+import aiohttp
+import pytest
+
+from functional.settings import test_settings
+
+
+@pytest.fixture
+def make_get_request():
+    """
+    Fixture for making GET requests to the fastapi service
+    """
+    async def inner(url: str, params: dict = None):
+        async with aiohttp.ClientSession() as session:
+            async with session.get(test_settings.service_url + '/api/v1' + url, params=params) as response:
+                return {
+                    'status': response.status,
+                    'json': await response.json()
+                }
+    return inner
