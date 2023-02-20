c76fa83eefae142bd85dd8c98c0ab673f1a4b419
salliko <snup41@yandex.ru>
2023-02-21 00:01:36 +0300

    create auth routers

--- auth-solution/src/extensions.py
+++ auth-solution/src/extensions.py
@@ -1,3 +1,5 @@
+import redis
+
 from flask_jwt_extended import JWTManager
 from flask_security import SQLAlchemyUserDatastore, Security
 
@@ -8,3 +10,11 @@ from .models import db, User, Role
 jwt = JWTManager()
 user_datastore = SQLAlchemyUserDatastore(db, User, Role)
 security = Security(datastore=user_datastore)
+
+jwt_redis_blocklist = redis.StrictRedis(
+    host="localhost", port=6379, db=0, decode_responses=True
+)
+
+jwt_redis_refresh_tokens = redis.StrictRedis(
+    host="localhost", port=6379, db=1, decode_responses=True
+)
