1361f858c3e033895e03414a3415b996d963d832
vimjj <vimjj@yandex.ru>
2023-02-23 21:11:27 +0600

    Minor stylistic fixes

--- auth-solution/src/api/v1/roles.py
+++ auth-solution/src/api/v1/roles.py
@@ -34,19 +34,19 @@ def role():
         name = request.json["name"]
         _role = user_datastore.find_role(role=name)
         if _role:
-            return jsonify('Такая роль уже существует')
+            return jsonify(f'{_role} роль уже существует')
         user_datastore.create_role(name=name)
         user_datastore.commit()
-        return jsonify('Role created')
+        return jsonify(f'Role {name} created')
 
     if request.method == 'DELETE':
         name = request.json["name"]
         _role = user_datastore.find_role(role=name)
         if not _role:
-            return jsonify('Роль отсутствует')
+            return jsonify(f'Роль {_role} отсутствует')
         user_datastore.delete(_role)
         user_datastore.commit()
-        return jsonify('Role deleted')
+        return jsonify(f'Role {name} deleted')
 
 
 @bp.route('/user/<id>', methods=('GET', 'POST', 'DELETE'))
@@ -57,21 +57,28 @@ def user(id):
     if not has_role('admin'):
         return jsonify('Access is denied'), 403
 
+    _user = user_datastore.find_user(id=id)
+    if not _user:
+        return jsonify(f'{_user} is missing')
+
     if request.method == 'GET':
-        _user = user_datastore.find_user(id=id)
         return jsonify(_user.roles)
 
     if request.method == 'POST':
         role_name = request.json["role_name"]
-        _user = user_datastore.find_user(id=id)
         _role = user_datastore.find_role(role=role_name)
+        if not _role:
+            return jsonify(f'Role {_role} is missing')
+
         user_datastore.add_role_to_user(_user, _role)
         user_datastore.commit()
-        return jsonify('role added success')
+        return jsonify(f'Role {role_name} successfully added to {_user}')
 
     if request.method == 'DELETE':
         role_name = request.json["role_name"]
-        _user = user_datastore.find_user(id=id)
         _role = user_datastore.find_role(role=role_name)
+        if not _role:
+            return jsonify(f'Role {_role} is missing')
+
         user_datastore.remove_role_from_user(_user, _role)
-        return jsonify('role removed success')
+        return jsonify(f'Role {role_name} successfully removed from {_user}')
