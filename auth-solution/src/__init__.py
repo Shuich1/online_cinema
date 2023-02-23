25ade1bf42c02be0b5cf21a9c2bff787c48cb2eb
vimjj <vimjj@yandex.ru>
2023-02-23 21:09:51 +0600

    Added admin user creation

--- auth-solution/src/__init__.py
+++ auth-solution/src/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from flask_security.utils import hash_password
 
 from .api.v1 import auth, roles
 from .core.config import settings
 from .services.database import db
 from .utils.extensions import jwt, security
+from src.utils.extensions import user_datastore
 
 
 def create_app():
@@ -17,11 +19,22 @@ def create_app():
     app.register_blueprint(roles.bp)
 
     db.init_app(app)
+
     with app.app_context():
         app.logger.info('initialised database.')
         db.create_all()
+        security.init_app(app)
+        jwt.init_app(app)
 
-    security.init_app(app)
-    jwt.init_app(app)
+        if not user_datastore.find_role(role='admin'):
+            user_datastore.create_role(name='admin')
+            user_datastore.commit()
+        admin_user = user_datastore.find_user(email=settings.FLASK_ADMIN_MAIL)
+        if not admin_user:
+            user_datastore.create_user(email=settings.FLASK_ADMIN_MAIL,
+                                       password=hash_password(settings.FLASK_ADMIN_PASS),
+                                       roles=['admin']
+                                       )
+            user_datastore.commit()
 
     return app
