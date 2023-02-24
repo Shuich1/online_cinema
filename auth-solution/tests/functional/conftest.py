c6f40db6d039f2d1db0add4e3de84e960a27033a
Shuichi <zhuravkov.mai@gmail.com>
2023-02-24 14:32:27 +0300

    Auth tests

--- auth-solution/tests/functional/conftest.py
+++ auth-solution/tests/functional/conftest.py
@@ -4,19 +4,28 @@ import aiohttp
 import pytest
 
 from .settings import test_settings
+from .testdata.users_data import test_user
 
 
 @pytest.fixture
-def make_get_request():
+def make_request():
     """
-    Fixture for making GET requests to the auth service
+    Fixture for making requests to the auth service
     """
-    async def inner(url: str, params: dict = None, headers: dict = None):
+    async def inner(
+        method: str,
+        url: str,
+        payload: dict = None,
+        params: dict = None,
+        headers: dict = None
+    ):
         async with aiohttp.ClientSession() as session:
-            async with session.get(
+            async with session.request(
+                method,
                 test_settings.service_url + url,
+                json=payload,
                 params=params,
-                headers = headers
+                headers=headers
             ) as response:
                 return {
                     'status': response.status,
@@ -27,23 +36,18 @@ def make_get_request():
 
 
 @pytest.fixture
-def make_post_request():
-    """
-    Fixture for making POST requests to the auth service
-    """
-    async def inner(url: str, payload: dict = None, headers: dict = None):
-        async with aiohttp.ClientSession() as session:
-            async with session.post(
-                test_settings.service_url + url,
-                json = payload,
-                headers = headers,
-            ) as response:
-                return {
-                    'status': response.status,
-                    'json': await response.json(),
-                    'headers': response.headers
-                }
-    return inner
+@pytest.mark.asyncio
+async def sign_in(make_request):
+    response = await make_request(
+        'POST',
+        '/auth/signin',
+        payload=test_user.dict()
+    )
+
+    return {
+        'access_token': response['headers']['Authorization'],
+        'refresh_token': response['json']['refresh_token']
+    }
 
 
 @pytest.fixture(scope='session', autouse=True)
