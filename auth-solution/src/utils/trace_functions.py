6ef7aa62416e8305b0cbaf3b328668483125d428
Shuichi <zhuravkov.mai@gmail.com>
2023-03-05 21:57:07 +0300

    updated traced decorator

--- auth-solution/src/utils/trace_functions.py
+++ auth-solution/src/utils/trace_functions.py
@@ -1,9 +1,19 @@
 from opentelemetry import trace
+from functools import wraps
+import asyncio
 
 
 def traced(func):
-    def inner(*args, **kwargs):
-        tracer = trace.get_tracer(__name__)
-        with tracer.start_as_current_span(func.__name__):
-            return func(*args, **kwargs)
+    if not asyncio.iscoroutinefunction(func):
+        @wraps(func)
+        def inner(*args, **kwargs):
+            tracer = trace.get_tracer(__name__)
+            with tracer.start_as_current_span(func.__name__):
+                return func(*args, **kwargs)
+    else:
+        @wraps(func)
+        async def inner(*args, **kwargs):
+            tracer = trace.get_tracer(__name__)
+            with tracer.start_as_current_span(func.__name__):
+                return await func(*args, **kwargs)
     return inner
