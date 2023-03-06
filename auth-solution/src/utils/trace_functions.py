7e29d885419732ed259823f6c21a2cdd155b25e4
Shuichi <zhuravkov.mai@gmail.com>
2023-03-07 02:22:32 +0300

    Added spans naming in traced decorator function

--- auth-solution/src/utils/trace_functions.py
+++ auth-solution/src/utils/trace_functions.py
@@ -4,17 +4,19 @@ from functools import wraps
 from opentelemetry import trace
 
 
-def traced(func):
-    if not asyncio.iscoroutinefunction(func):
-        @wraps(func)
-        def inner(*args, **kwargs):
-            tracer = trace.get_tracer(__name__)
-            with tracer.start_as_current_span(func.__name__):
-                return func(*args, **kwargs)
-    else:
-        @wraps(func)
-        async def inner(*args, **kwargs):
-            tracer = trace.get_tracer(__name__)
-            with tracer.start_as_current_span(func.__name__):
-                return await func(*args, **kwargs)
-    return inner
+def traced(name: str = None):
+    def wrapper(func):
+        if not asyncio.iscoroutinefunction(func):
+            @wraps(func)
+            def inner(*args, **kwargs):
+                tracer = trace.get_tracer(__name__)
+                with tracer.start_as_current_span(func.__name__ if not name else name):
+                    return func(*args, **kwargs)
+        else:
+            @wraps(func)
+            async def inner(*args, **kwargs):
+                tracer = trace.get_tracer(__name__)
+                with tracer.start_as_current_span(func.__name__ if not name else name):
+                    return await func(*args, **kwargs)
+        return inner
+    return wrapper
