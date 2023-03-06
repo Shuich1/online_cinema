7a6646aa021f60db6e136b4532e18ad489584b5c
grebennikov_iy <grebennikov_iy@magnit.ru>
2023-03-07 01:15:27 +0300

    добавил oauth сервис яндекса

---
+++ auth-solution/src/models/social_account.py
@@ -0,0 +1,20 @@
+import uuid
+
+from src.services.database import db
+from .user import User
+
+
+class SocialAccount(db.Model):
+    __tablename__ = 'social_account'
+
+    id = db.Column(db.UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True, nullable=False)
+    user_id = db.Column(db.UUID(as_uuid=True), db.ForeignKey('users.id'), nullable=False)
+    user = db.relationship(User, backref=db.backref('social_accounts', lazy=True))
+
+    social_id = db.Column(db.Text, nullable=False)
+    social_name = db.Column(db.Text, nullable=False)
+
+    __table_args__ = (db.UniqueConstraint('social_id', 'social_name', name='social_pk'),)
+
+    def __repr__(self):
+        return f'<SocialAccount {self.social_name}:{self.user_id}>'
